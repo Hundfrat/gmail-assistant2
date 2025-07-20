b5a6528505a89b8350b87685ed1a23d9fd183d31
Андрей <spuhu1234@gmail.com>
2025-07-20 08:48:42 +0300

    h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
-'use client';
+import { buttonVariants } from '@/components/ui/button';
+import User from '@/components/ui/User';
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import Link from 'next/link';
 
-import { useEffect } from 'react';
+export default async function Home() {
+  
+  const session = await getServerSession(authOptions);
+  
+  return (
+    
+    <div>
+      <h1 className='text-4xl'>Home</h1>
+      <Link className={buttonVariants()} href='/admin'>
+        Open My Admin
+      </Link>
 
-export default function Home() {
-  useEffect(() => {
-    window.location.href = 'https://gmail-assistant.com';
-  }, []);
+      <h2>Client Session</h2>
+      <User />
+      <h2>Server Session</h2>
+      {JSON.stringify(session)}
 
-  return null;
+    </div>
+  );
 }
